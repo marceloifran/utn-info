f187d356a1ae1d3c9e747486d51fda6a9751ee08
Marce Ifran Singh <64060640+marceloifran@users.noreply.github.com>
2022-04-09 11:21:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ if (res.status === 200){
 
     for (const i in datos){
         console.log(datos[i])
-        const {email1, email2,id,clave,materia,profe1,profe2,primerp,segundop} = datos[i]
+        const {email1, email2,id,clave,materia,profe1,profe2,primerp,segundop,infoPrimer,infoSegundo} = datos[i]
         card.innerHTML  += `
         <div class="card-group">
         <div class="card text-center">
@@ -22,16 +22,41 @@ if (res.status === 200){
             <p class="card-text">ID de reunión: ${id}</p>
             <p class="card-text"> Código de acceso: ${clave}</p>
             <p class="card-text">PARCIALES:</p>
-            <p class="card-text">1er Parcial ${primerp}</p>
-            <p class="card-text">2do Parcial ${segundop}</p>
-            <a href="mailto:${email1}" class="card-link">Profe ${profe1}</a>
-            <a href="mailto:${email2}" class="card-link">Profe ${profe2}</a>
+            <div class="accordion" id="accordionPanelsStayOpenExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header text-center" id="panelsStayOpen-headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                 ${primerp}
+                </button>
+              </h2>
+              <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                <div class="accordion-body">
+                  ${infoPrimer}
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                 ${segundop}
+                </button>
+              </h2>
+              <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                <div class="accordion-body">
+                 ${infoSegundo}
+                </div>
+              </div>
+            </div>
+          </div>
+            <a href="mailto:${email1}" class="btn btn-outline-primary">Profe ${profe1}</a>
+            <a href="mailto:${email2}" class="btn btn-outline-primary">Profe ${profe2}</a>
           </div>
          
       </div>
         `
         
     }
+    
 }
     }
     catch (err) {
