9d3412b8e6f75c6eafd240875fa7abaf0950c175
Marce Ifran Singh <64060640+marceloifran@users.noreply.github.com>
2022-04-09 11:38:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ if (res.status === 200){
         console.log(datos[i])
         const {email1, email2,id,clave,materia,profe1,profe2,primerp,segundop,infoPrimer,infoSegundo} = datos[i]
         card.innerHTML  += `
-        <div class="card-group">
+        <div class="card-group card text-dark bg-info mb-3">
         <div class="card text-center">
           <div class="card-body">
             <h5 class="card-title">${materia}</h5>
@@ -22,32 +22,10 @@ if (res.status === 200){
             <p class="card-text">ID de reunión: ${id}</p>
             <p class="card-text"> Código de acceso: ${clave}</p>
             <p class="card-text">PARCIALES:</p>
-            <div class="accordion" id="accordionPanelsStayOpenExample">
-            <div class="accordion-item">
-              <h2 class="accordion-header text-center" id="panelsStayOpen-headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                 ${primerp}
-                </button>
-              </h2>
-              <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                <div class="accordion-body">
-                  ${infoPrimer}
-                </div>
-              </div>
-            </div>
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                 ${segundop}
-                </button>
-              </h2>
-              <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-                <div class="accordion-body">
-                 ${infoSegundo}
-                </div>
-              </div>
-            </div>
-          </div>
+            <p class="card-text"> ${primerp}</p><p class="card-text">${infoPrimer}</p>
+           
+            <p class="card-text"> ${segundop}</p>
+            <p class="card-text">${infoSegundo}</p>
             <a href="mailto:${email1}" class="btn btn-outline-primary">Profe ${profe1}</a>
             <a href="mailto:${email2}" class="btn btn-outline-primary">Profe ${profe2}</a>
           </div>
